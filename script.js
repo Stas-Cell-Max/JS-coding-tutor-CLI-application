401aad010181b25d062e6b618c1c77fdcedc3cd5
Stanislav Morozan <stanislav.morozan@gmail.com>
2023-12-02 15:44:25 -0500

    instantiate a new object using the OpenAI class

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 require('dotenv').config();
+
 console.log(process.env.OPENAI_API_KEY);
+
 const { OpenAI } = require('langchain/llms/openai');
+
+const model = new OpenAI({ 
+    openAIApiKey: process.env.OPENAI_API_KEY, 
+    temperature: 0,
+    model: 'gpt-3.5-turbo'
+  });
+  
+  console.log({ model });
+  
+

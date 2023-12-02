64e3557d30710305c5e89987e144d9f4a5f9475c
Stanislav Morozan <stanislav.morozan@gmail.com>
2023-12-02 16:16:04 -0500

    Created an OpenAI wrapper, model; function that uses inquirer to prompt the user

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+
+// dependencies
+const { OpenAI } = require('langchain/llms/openai');
 require('dotenv').config();
 
-console.log(process.env.OPENAI_API_KEY);
 
-const { OpenAI } = require('langchain/llms/openai');
-
+// Creates and stores a wrapper for the OpenAI package along with basic configuration
 const model = new OpenAI({ 
     openAIApiKey: process.env.OPENAI_API_KEY, 
     temperature: 0,
@@ -11,5 +12,35 @@ const model = new OpenAI({
   });
   
   console.log({ model });
+
+
+// Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
+  const promptFunc = async () => {
+    try {
+        const res = await model.call(input);
+        console.log(res);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  };
+
+  const inquirer = require('inquirer');
+
+ // function that, when called, will execute an inquirer prompt that will ask the user for a question
+  const init = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Ask a coding question:',
+      },
+    ]).then((inquirerResponse) => {
+      promptFunc(inquirerResponse.name)
+    });
+  };
   
+  // Calls the initialization function and starts the script
+  init();
+
 

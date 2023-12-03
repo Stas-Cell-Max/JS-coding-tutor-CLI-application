7301a1303aa2cb7c4fa61bbd7098076bb04879dc
Stanislav Morozan <stanislav.morozan@gmail.com>
2023-12-03 16:14:31 -0500

    Implemented StructuredOutputParser, added a new property to prompt object

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
+// Dependencies
+require("dotenv").config();
+const inquirer = require("inquirer");
+const { OpenAI } = require("langchain/llms/openai");
 
-// dependencies
-const { OpenAI } = require('langchain/llms/openai');
-require('dotenv').config();
-
+// The following dependencies are not required for this script to run, but are used to format the output
+const { PromptTemplate } = require("langchain/prompts");
+const { StructuredOutputParser } = require("langchain/output_parsers");
 
 // Creates and stores a wrapper for the OpenAI package along with basic configuration
-const model = new OpenAI({ 
-    openAIApiKey: process.env.OPENAI_API_KEY, 
-    temperature: 0,
-    model: 'gpt-3.5-turbo'
-  });
+const model = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0,
+  model: "gpt-3.5-turbo",
+});
+
+// With a 'StructuredOutputParser' we can define a schema for the output     
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+      // Define the output variables and their descriptions
+    code: "Javascript code that answers the user's question",       
+    explanation: "detailed explanation of the example code provided",     
+  });    
+  // Get the format instructions from the parser 
+  const formatInstructions = parser.getFormatInstructions();
   
-  console.log({ model });
-
-
-// Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
-const promptFunc = async (input) => { // Add 'input' as a parameter
+  const promptFunc = async (input) => {
     try {
-      const res = await model.call(input);
-      console.log(res);
+      const prompt = new PromptTemplate({
+        template:
+          "You are a javascript expert and will answer the user's coding questions thoroughly as possible. \n{format_instructions}",
+        inputVariables: ["question"],
+        partialVariables: { format_instructions: formatInstructions },
+      });
+  
+      const promptInput = await prompt.format({ question: input });
+      
+     
+  
+      const res = await model.call(promptInput);
+      const parsedResult = await parser.parse(res);
+  
+      // Log the parsed result for debugging purposes
+      console.log("Parsed Result:", parsedResult);
     } catch (err) {
-      console.error(err);
+      console.error("Error during promptFunc:", err);
     }
-  
-    // Instantiation of a new object called "prompt" using the "PromptTemplate" class
-    const prompt = new PromptTemplate({
-      template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{question}",
-      inputVariables: ["question"],
-    });
-  
-    const promptInput = await prompt.format({
-      question: input
-    });
   };
- 
-
-  const inquirer = require('inquirer');
-
- // function that, when called, will execute an inquirer prompt that will ask the user for a question
-  const init = () => {
-    inquirer.prompt([
+  
+// Initialization function that uses inquirer to prompt the user and returns a promise. It takes the user input and passes it through the call method
+const init = () => {
+  inquirer
+    .prompt([
       {
-        type: 'input',
-        name: 'name',
-        message: 'Ask a coding question:',
+        type: "input",
+        name: "name",
+        message: "Ask a coding question",
       },
-    ]).then((inquirerResponse) => {
-      promptFunc(inquirerResponse.name)
+    ])
+    .then((inquirerResponse) => {
+      promptFunc(inquirerResponse.name);
     });
-  };
-  
-  // Calls the initialization function and starts the script
-  init();
-
-
-  const { PromptTemplate } = require("langchain/prompts");
-
-  
-
+};
+// Calls the intialization function and starts the script
+init(); 
